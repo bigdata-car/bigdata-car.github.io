ffa2ec444f84c7d71a63747b632f245a96f626c1
bigdata-car <admin@bigdata-car.kr>
2024-07-05 10:53:53 +0900

    240705

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,12 +16,13 @@ const config = {
   url: "https://bigdata-car.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/kadap-docs/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "bigdata-car", // Usually your GitHub org/user name.
-  projectName: "kadap-docs", // Usually your repo name.
+  projectName: "bigdata-car.github.io", // Usually your repo name.
+  deploymentBranch: "deployment",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
